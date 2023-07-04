fb61071c9aa6eba0d336ac19ec1b22705d06dbbf
NGNikolay <georgiev.nikolaj@gmail.com>
2023-07-04 17:46:51 +0300

    Update app.js

--- 01-asynchronous-programming/02.Github-Repos/scripts/app.js
+++ 01-asynchronous-programming/02.Github-Repos/scripts/app.js
@@ -1,3 +1,27 @@
 function loadRepos() {
-	console.log("TODO...");
+	const input = document.getElementById('username');
+	let ulRepos = document.getElementById('repos');
+	let user = input.value;
+	let url = `https://api.github.com/users/${user}/repos`;
+
+	fetch(url)
+		.then(res => res.json())
+		.then(data => {
+			data.forEach(element => {
+
+				let li = createLi(element.full_name, element.html_url);
+				ulRepos.appendChild(li);
+			});
+		})
+
+
+	function createLi(name, url){
+		let li = document.createElement('li');
+		let a = document.createElement('a');
+		a.textContent = name;
+		a.href = url;
+		li.appendChild(a);
+		return li;
+	}
+
 }
