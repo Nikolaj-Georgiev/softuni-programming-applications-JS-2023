f4e6fd53fb411387c1e1ee0c031e7aa71d1b2583
NGNikolay <georgiev.nikolaj@gmail.com>
2023-07-05 13:57:23 +0300

    Update app.js

--- 02-asynchronous-programming-exersice/01.Bus-Stop/app.js
+++ 02-asynchronous-programming-exersice/01.Bus-Stop/app.js
@@ -1,3 +1,24 @@
 function getInfo() {
-    console.log("TODO...");
+    const stopId = document.getElementById('stopId');
+    const stopName = document.getElementById('stopName');
+    const ulBuses = document.getElementById('buses');
+
+    const baseURL = `http://localhost:3030/jsonstore/bus/businfo`;
+
+    fetch(`${baseURL}/${stopId.value}`)
+        .then(response => response.json())
+        .then(data => {
+            ulBuses.innerHTML ='';
+            stopName.textContent = data.name;
+            const buses = Object.keys(data.buses);
+            buses.forEach(bus => {
+                let li = document.createElement('li');
+                li.textContent = `Bus ${bus} arrives in ${data.buses[bus]} minutes`;
+                ulBuses.appendChild(li);
+            });
+        })
+        .catch(error => {
+            ulBuses.innerHTML = '';
+            stopName.textContent = 'Error';
+        })
 }
