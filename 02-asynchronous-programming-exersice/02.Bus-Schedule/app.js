d93ad215031cdce6e3223d79628c97629e22cf10
NGNikolay <georgiev.nikolaj@gmail.com>
2023-07-06 09:44:42 +0300

    Update app.js

--- 02-asynchronous-programming-exersice/02.Bus-Schedule/app.js
+++ 02-asynchronous-programming-exersice/02.Bus-Schedule/app.js
@@ -1,11 +1,35 @@
 function solve() {
 
+    const infoSign = document.querySelector('.info');
+    const departBtn = document.querySelector('#depart');
+    const arriveBtn = document.querySelector('#arrive');
+    infoSign.textContent = 'Babalugaaaa';
+
+    let busStop = {
+        next: 'depot'
+    }
+
     function depart() {
-        console.log('Depart TODO...');
+        const url = `http://localhost:3030/jsonstore/bus/schedule/${busStop.next}`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                busStop = JSON.parse(JSON.stringify(data));
+                infoSign.textContent = `Next stop ${busStop.name}`;
+                departBtn.disabled = true;
+                arriveBtn.disabled = false;
+            })
+            .catch(error => {
+                infoSign.textContent = 'Error';
+                departBtn.disabled = true;
+                arriveBtn.disabled = true;
+            })
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        infoSign.textContent = `Arriving at ${busStop.name}`;
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
     }
 
     return {
