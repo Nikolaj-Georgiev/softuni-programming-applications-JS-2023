110e05a0abfc2e555499b8ae1fbd7dcee820c112
NGNikolay <georgiev.nikolaj@gmail.com>
2023-07-28 15:07:39 +0300

    solution

---
+++ 09-exam-prep/clear-career/src/data/applications.js
@@ -0,0 +1,19 @@
+import { get, post } from "./api.js";
+
+const endpoints = {
+    applications: '/data/applications',
+    byOfferId: offerId => `/data/applications?where=offerId%3D%22${offerId}%22&distinct=_ownerId&count`,
+    byOfferIdAndUserId: (offerId, userId) => `/data/applications?where=offerId%3D%22${offerId}%22%20and%20_ownerId%3D%22${userId}%22&count`
+};
+
+export async function apply(offerId){
+    return post(endpoints.applications, {offerId});
+}
+
+export async function getApplications(offerId){
+    return get(endpoints.byOfferId(offerId));
+}
+
+export async function getUserApplication(offerId, userId) {
+    return get(endpoints.byOfferIdAndUserId(offerId, userId));
+}
